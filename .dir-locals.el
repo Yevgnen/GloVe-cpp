5ddd7d2d8f82ed0dfe5356fa6961f4cff379ad89
Jiajian Huang <wherejoystarts@gmail.com>
2017-12-21 14:18:24 +0800

    Update default compile command, use more threads.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
-  (projectile-project-compilation-cmd . "cd build && cmake .. -G \"Unix Makefiles\" -DCMAKE_BUILD_TYPE=Release && cmake --build .")))
+  (projectile-project-compilation-cmd . "cd build && cmake .. -G \"Unix Makefiles\" -DCMAKE_BUILD_TYPE=Release && cmake --build . -- -j 6")))
 
